082ac926337f53b11a33dcf8de7413cadbe90530
Alyssa <alyssadanijones@gmail.com>
2019-10-20 10:58:22 -0400

    pseudocode for general outline of weather manager js file

---
+++ js/weather-manager.js
@@ -0,0 +1,22 @@
+/*
+	use dark skies API to add weather info to site
+*/
+
+/* TODO
+	pull relevant elements from document
+		- where data will be added
+
+	populate document with data
+		- API call (GET)
+			- save
+				current temp
+				min/max
+				%chance of precipitation
+				hourly temp
+				hourly precipitation(?)
+				humidity (hourly?)
+				wind speed (hourly?)
+			- create plots based on data (using d3.js)
+			- update document with data/plots
+			- suggest outfit based on weather report
+*/
